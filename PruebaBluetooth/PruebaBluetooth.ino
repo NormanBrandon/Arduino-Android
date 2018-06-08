26e67fcc89037d831b61a027d90fa0a4049d75ec
NormanBrandon <saldananormanbrandon1iv18@gmail.com>
2018-06-07 19:30:45 -0500

    Programa d prueba para la aplicacion

--- PruebaBluetooth/PruebaBluetooth.ino
+++ PruebaBluetooth/PruebaBluetooth.ino
@@ -1,22 +1,26 @@
-void setup()  
-{
-  Serial.begin(9600);
-  pinMode(13,OUTPUT);
-  pinMode(A0,OUTPUT);
+#include <SoftwareSerial.h>
+
+int ledPin = 13; // usamos un pin de salida al LED
+char state = 0; // Variable lectrura serial
+
+SoftwareSerial mySerial(10, 11);//conecta el tx del bluetooth al 10 y el rx al 11
+void setup() {
+    pinMode(ledPin, OUTPUT);   //Declara pin de Salida
+    digitalWrite(ledPin, LOW); //Normalmente Apagado
+    Serial.begin(9600);
+    mySerial.begin(9600);
+    delay(500);
+    mySerial.println("Hello, world?");
 }
-
-void loop() 
-{
-  if (Serial.available())
-  {
-    char dato=Serial.read();
-    digitalWrite(13,HIGH);
-    digitalWrite(A0,HIGH);
-   
-    delay(200);
-    Serial.println('a');
-  }
-  digitalWrite(13,LOW);
-  digitalWrite(A0,LOW);
-
+ 
+void loop() {
+ //si el modulo a manda dato, guardarlo en estado.
+  if(Serial.available() > 0){
+       state = mySerial.read();
+       Serial.println(state);
+ 
+  } // esta parte del código es para solo 1 Carácter o Unidad. 
+ 
+ 
+ 
 }
